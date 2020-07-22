0ac75c221c315097d0f32c7d126fffc2db098b3c
Nestuna <nicolasantunespereira@gmail.com>
2020-07-22 18:03:40 +0200

    Ajout dans BDD via console

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -21,25 +21,47 @@ ALBUMS = [
 
 ### MODELES SQL ------------
 
-class Artists(models.Model):
+class Artist(models.Model):
+  # Attribut
   name = models.CharField(max_length=200, unique=True)
 
+  # Méthodes
+  def __str__(self):
+    return self.name
+
 class Contact(models.Model):
+  # Attributs
   name = models.CharField(max_length=200, unique=True)
   email = models.EmailField(max_length=200, unique=True)
 
+  # Méthodes
+  def __str__(self):
+    return self.name
+
 class Album(models.Model):
+  # Attributs
   reference = models.IntegerField(null=True)
   created_at = models.DateField(auto_now_add=True)
   available = models.BooleanField(default=True)
   title = models.CharField(max_length=200)
   picture= models.URLField()
 
+  # Relations 
+  artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+
+  # Méthodes
+  def __str__(self):
+    return self.title
+
 class Booking(models.Model):
+  # Attributs
   created_at = models.DateField(auto_now_add=True)
   contacted = models.BooleanField(default=False)
   
   # Relations
   album = models.OneToOneField(Album, on_delete= models.DO_NOTHING) # Une reservation par album
   contact = models.ForeignKey(Contact, on_delete=models.CASCADE) # Plusieurs réservation peuvent appartenir à un contact
-  artists = models.ManyToManyField(Album, related_name='albums', blank=True)
+  
+  # Méthodes
+  def __str__(self):
+    return self.contact.name
