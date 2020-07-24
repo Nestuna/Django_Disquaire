c4ba66f735ea507dd93951fbd6a4c36f7296224a
Nestuna <nicolasantunespereira@gmail.com>
2020-07-24 10:30:00 +0200

    page details

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'store'
 urlpatterns = [
    path('', views.listing, name='listing'),
    path('<int:album_id>/', views.details, name='details'),
--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Album, Artist, Contact, Booking
 
 # Create your views here.
 def index(request):
-   albums = Album.objects.filter(available=True).order_by('-created_at')[:2]
+   albums = Album.objects.filter(available=True).order_by('-created_at')
    formatted_albums = ['<li>{}</li>'.format(album.title) for album in albums]
-   message = """
-      Les deux premiers albums : 
-      <ul>
-         {}
-      </ul>
-   """.format("\n".join(formatted_albums))
-   return HttpResponse(message)
+   # message = """
+   #    Les deux premiers albums : 
+   #    <ul>
+   #       {}
+   #    </ul>
+   # """.format("\n".join(formatted_albums))
+
+   template = loader.get_template('store/index.html')
+   context = {
+      'albums': albums
+   }
+   return render(request, 'store/index.html', context)
 
 def listing(request):
    albums = Album.objects.filter(available=True).order_by('-created_at')
@@ -28,10 +34,12 @@ def listing(request):
 
 def details(request, album_id):
    album = Album.objects.get(pk=album_id)
-   artists =  ", ".join(artist.name for artist in album.artists.all()).capitalize()
-
-   message = f"Le nom de l'album est {album.title}. Il a été écrit par {artists}"
-   return HttpResponse(message)
+   artists = ", ".join(artist.name for artist in album.artists.all()).capitalize()
+   context = {
+      'album' : album,
+      'artists' : artists
+   }
+   return render(request, 'store/details.html', context)
 
 def search(request):
    query = request.GET.get('query')
